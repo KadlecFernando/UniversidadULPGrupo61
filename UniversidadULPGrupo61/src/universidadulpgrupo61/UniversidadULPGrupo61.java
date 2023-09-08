68b05648ac101ec76eb4c03ab2be8df936733b73
KadlecFernando <kadlec.fernando13@gmail.com>
2023-09-08 15:13:27 -0300

    SegundoCommit

--- UniversidadULPGrupo61/src/universidadulpgrupo61/UniversidadULPGrupo61.java
+++ UniversidadULPGrupo61/src/universidadulpgrupo61/UniversidadULPGrupo61.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadulpgrupo61;
 
-/**
- *
- * @author ferge
- */
+
 public class UniversidadULPGrupo61 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
--- UniversidadULPGrupo61/src/universidadulpgrupo61/entidades/Alumno.java
+++ UniversidadULPGrupo61/src/universidadulpgrupo61/entidades/Alumno.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadulpgrupo61.entidades;
 
-/**
- *
- * @author ferge
- */
+
 public class Alumno {
     
 }
