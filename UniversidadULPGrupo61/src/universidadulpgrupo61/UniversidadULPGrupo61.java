519bfb919cc8c98bbca1f8b39184fcf3fc5ba759
KadlecFernando <kadlec.fernando13@gmail.com>
2023-09-11 17:44:29 -0300

    Testeandoando

--- UniversidadULPGrupo61/src/universidadulpgrupo61/UniversidadULPGrupo61.java
+++ UniversidadULPGrupo61/src/universidadulpgrupo61/UniversidadULPGrupo61.java
@@ -1,12 +1,22 @@
 
 package universidadulpgrupo61;
 
+import java.time.LocalDate;
+import universidadulpgrupo61.entidades.Alumno;
+import universidadulpgrupo61.entidades.accesoADatos.AlumnoData;
+
 
 public class UniversidadULPGrupo61 {
 
     
     public static void main(String[] args) {
-        //hola aaaa
+        AlumnoData maria = new AlumnoData();
+       Alumno alum = new Alumno(65347345,"apellido","nombre",LocalDate.of(1996, 11, 6),true);
+       maria.guardarAlumno(alum);
+       
+       
+       
+       
     }
     
 }
