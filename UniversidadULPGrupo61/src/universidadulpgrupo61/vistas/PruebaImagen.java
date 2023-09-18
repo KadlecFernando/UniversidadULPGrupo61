65d7c174a16f849dcf3788ea4e4dcae94ce56da4
KadlecFernando <kadlec.fernando13@gmail.com>
2023-09-18 16:18:18 -0300

    --

--- UniversidadULPGrupo61/src/universidadulpgrupo61/vistas/PruebaImagen.java
+++ UniversidadULPGrupo61/src/universidadulpgrupo61/vistas/PruebaImagen.java
@@ -41,11 +41,11 @@ public class PruebaImagen extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 749, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 519, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
